7fa726f2e8fbac57bacd1e5d380b16c788c2db1a
Harlocchi <gustaalves1701@gmail.com>
2024-07-25 21:28:44 -0300

    puxando pessoa da api

--- lib/credentials.dart
+++ lib/credentials.dart
@@ -1,16 +1,16 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class credential{
 
 
-  static String DOMAIN = 'https://manager.cpcon.co';
+  static String DOMAIN = 'https://sandbox.cpcon.co';
   static String DomainJWT = '$DOMAIN/api/jwt/obtain-token/';
   static String APPVERSION = "2.4.1";
 
   credential();
-
    Future<http.Response> getTokenJWT(user, password) async {
       var url = Uri.parse(DomainJWT);
 
@@ -27,7 +27,47 @@ class credential{
         url, headers: header, body: body
       );
 
+      var decode = jsonDecode(response.body);
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setString("jwt", decode["token"]);
       return response;
   }
 
+  Future<String> getTokenSharedPref() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String token = prefs.getString("jwt") ?? '';
+     return token;
+  }
+
+  Future<List<Map<String, dynamic>?>> getPeople(String code) async {
+    var url = Uri.parse("$DOMAIN/api/people-management/?identify_number=$code");
+
+    String token = await getTokenSharedPref();
+
+    print("CODE $code");
+    print("TOKEN $token");
+    print("$DOMAIN/api/people-management/?identify_number=$code");
+
+    var header = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $token',
+      'x-tenant':"b3a0b746-1b53-4346-b9a4-7b6a8ee9588e"
+    };
+
+    var response = await http.get(
+        url, headers: header
+    );
+
+
+    List<Map<String, dynamic?>> results = [];
+    var decode = jsonDecode(response.body);
+
+    for(var people in decode['results']){
+      results.add(people);
+    }
+    
+    Map<String, dynamic> toReturn = {};
+    print(results);
+    return results;
+  }
 }
--- lib/screens/WorkSpace.dart
+++ lib/screens/WorkSpace.dart
@@ -1,66 +1,306 @@
 import 'package:barcode_scan2/barcode_scan2.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:qrcode/utilsStyled/Inputs.dart';
+
+import '../credentials.dart';
 
 
-class Workspace extends StatelessWidget {
+class Workspace extends StatefulWidget {
   const Workspace({super.key});
 
   @override
+  State<Workspace> createState() => _WorkspaceState();
+}
+
+class _WorkspaceState extends State<Workspace> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            flex: 5,
-            child: Container(
-              width: double.infinity,
-              color: Colors.white, // Fundo branco
-              child: const qrCodeButton(),
-            ),
-          ),
-        ],
+      body: SingleChildScrollView(
+        child: Container(
+          width: double.infinity,
+          color: Colors.white, // Fundo branco
+          child: const QrCodeButton(),
+        ),
       ),
     );
   }
 }
 
-class qrCodeButton extends StatefulWidget {
-  const qrCodeButton({super.key});
+class QrCodeButton extends StatefulWidget {
+  const QrCodeButton({super.key});
 
   @override
-  State<qrCodeButton> createState() => _qrCodeButtonState();
+  State<QrCodeButton> createState() => _QrCodeButtonState();
 }
 
-class _qrCodeButtonState extends State<qrCodeButton> {
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-        flex: 5,
-        child: Container(
-          width: double.infinity,
-          decoration: const BoxDecoration(
-          ),
-          child: Column(
-            children: [
+class _QrCodeButtonState extends State<QrCodeButton> {
 
-              Container(
-                width: 100, // Largura do quadrado
-                height: 100, // Altura do quadrado
-                color: Colors.indigo, // Cor de fundo
-              ),
-            ],
-          ),
-        )
-    );
+  credential cred = credential();
+  String _code = "";
+  Map<String, dynamic>? _data = {};
+  Future<List<Map<String, dynamic>?>> futureData = Future.value([]);
+
+  late TextEditingController _controller;
+   void _editVarQrCode() async{
+      String read = "";
+      try {
+        ScanResult qr = await BarcodeScanner.scan();
+        read = qr.rawContent;
+        print(read);
+      } catch (e) {
+        read = "";
+      }
+       setState(() {
+         _code = read;
+         _controller.text = _code;
+       });
   }
-  Future<String> getCode() async{
-    String read = "";
-    try {
-      ScanResult qr = await BarcodeScanner.scan();
-      read = qr.rawContent;
-    } catch (e) {
-      read = "";
-    }
-    return read;
+
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController(text: _code);
+  }
+
+  Widget build(BuildContext context) {
+
+    return Container(
+      width: double.infinity,
+      decoration: const BoxDecoration(
+      ),
+      child: Column(
+        children: [
+          Padding(padding: const EdgeInsets.fromLTRB(0, 80, 0, 0),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                elevation: 10,
+                padding: EdgeInsets.zero, // Remove o padding padrão do botão
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10), // Define o raio das bordas
+                ),
+              ),
+              child:  Container(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  color: Colors.indigo,
+                ),
+                width: 100,
+                height: 100,
+                child: const Center(
+                  child: Icon(
+                    Icons.qr_code_scanner_sharp,
+                    color: Colors.white,
+                    size: 80,
+                  ),
+                ),
+              ),
+              onPressed: (){
+                 _editVarQrCode();
+              },
+            ),
+          ),
+
+          Padding(padding: EdgeInsets.fromLTRB(40, 20, 40, 0),
+            child: CustomTextFieldDecoration(
+              child:TextField(
+                controller: _controller,
+                onChanged: (value){
+                  setState(() {
+                    _code = value;
+                  });
+                },
+                style: const TextStyle(
+                    color: Color(0xff03d4ab1),
+                    fontFamily: "ProductSansRegular"
+                ),
+                decoration: InputDecoration(
+                  labelText: "Codigo da Pulseira",
+                  suffixIcon: IconButton(
+                    icon: const Icon(
+                      Icons.search,
+                      color: Color(0xff03d4ab1),
+                    ),
+                    onPressed: () async {
+                      setState(() {
+                        futureData = cred.getPeople(_code);
+                      });
+                      /*setState(() async {
+                        _data = await cred.getPeople(_code);
+                      });*/
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ),
+          FutureBuilder(future: futureData, builder: (context, snap){
+
+            if(snap.connectionState == ConnectionState.waiting){
+              return CircularProgressIndicator(
+                color: Colors.amber,
+              );
+            }
+
+            List<Map<String, dynamic>?> results = snap.data??[];
+            if(snap.hasError){
+              return Text("Não deu não vei");
+            }
+
+            if(results.isEmpty){
+              return Text("ta vazio vein");
+            }
+
+            return Column(
+              children: [
+                Container(
+                  alignment: Alignment.topLeft,
+                  child: const Padding(padding: EdgeInsets.fromLTRB(40, 20, 40, 0),
+                    child: Text(
+                      "nome:",
+                      style: TextStyle(
+                          color: Color(0xff03d4ab1),
+                          fontFamily: "ProductSansBold"
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.topLeft,
+                  child:  const Padding(padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
+                    child: Text(
+                      'oi',
+                      style: const TextStyle(
+                          color: Color(0xff03d4ab1),
+                          fontFamily: "ProductSansBold"
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.topLeft,
+                  child: const Padding(padding: EdgeInsets.fromLTRB(40, 20, 40, 0),
+                    child: Text(
+                      "nome:",
+                      style: TextStyle(
+                          color: Color(0xff03d4ab1),
+                          fontFamily: "ProductSansBold"
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.topLeft,
+                  child: const Padding(padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
+                    child: Text(
+                      "",
+                      style: TextStyle(
+                          color: Color(0xff03d4ab1),
+                          fontFamily: "ProductSansBold"
+                      ),
+                    ),
+                  ),
+                ),    Container(
+                  alignment: Alignment.topLeft,
+                  child: Padding(padding: EdgeInsets.fromLTRB(40, 20, 40, 0),
+                    child: Text(
+                      "${snap.data?[0]?['username']}",
+                      style: TextStyle(
+                          color: Color(0xff03d4ab1),
+                          fontFamily: "ProductSansBold"
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.topLeft,
+                  child: const Padding(padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
+                    child: Text(
+                      "ANNE MARTINS FERREIRA",
+                      style: TextStyle(
+                          color: Color(0xff03d4ab1),
+                          fontFamily: "ProductSansBold"
+                      ),
+                    ),
+                  ),
+                ),    Container(
+                  alignment: Alignment.topLeft,
+                  child: const Padding(padding: EdgeInsets.fromLTRB(40, 20, 40, 0),
+                    child: Text(
+                      "nome:",
+                      style: TextStyle(
+                          color: Color(0xff03d4ab1),
+                          fontFamily: "ProductSansBold"
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.topLeft,
+                  child: const Padding(padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
+                    child: Text(
+                      "ANNE MARTINS FERREIRA",
+                      style: TextStyle(
+                          color: Color(0xff03d4ab1),
+                          fontFamily: "ProductSansBold"
+                      ),
+                    ),
+                  ),
+                ),    Container(
+                  alignment: Alignment.topLeft,
+                  child: const Padding(padding: EdgeInsets.fromLTRB(40, 20, 40, 0),
+                    child: Text(
+                      "nome:",
+                      style: TextStyle(
+                          color: Color(0xff03d4ab1),
+                          fontFamily: "ProductSansBold"
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.topLeft,
+                  child: const Padding(padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
+                    child: Text(
+                      "ANNE MARTINS FERREIRA",
+                      style: TextStyle(
+                          color: Color(0xff03d4ab1),
+                          fontFamily: "ProductSansBold"
+                      ),
+                    ),
+                  ),
+                ),    Container(
+                  alignment: Alignment.topLeft,
+                  child: const Padding(padding: EdgeInsets.fromLTRB(40, 20, 40, 0),
+                    child: Text(
+                      "nome:",
+                      style: TextStyle(
+                          color: Color(0xff03d4ab1),
+                          fontFamily: "ProductSansBold"
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.topLeft,
+                  child: const Padding(padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
+                    child: Text(
+                      "ANNE MARTINS FERREIRA",
+                      style: TextStyle(
+                          color: Color(0xff03d4ab1),
+                          fontFamily: "ProductSansBold"
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            );
+          })
+
+        ],
+      ),
+    );
   }
 }
